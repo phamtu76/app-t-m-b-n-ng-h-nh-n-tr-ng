c24a8982bb044b741acaf1f056edc25806c51de5
phamtu76 <taolapro53241@gmail.com>
2026-05-16 22:24:47 +0700

    Refactor user registration to include route

--- main.cpp
+++ main.cpp
@@ -16,85 +16,57 @@ struct Route {
 vector<User> users;
 vector<Route> routes;
 
-void registerUser() {
+void registerUserAndRoute() {
     int id = users.size() + 1;
-    string name;
+    string name, dest;
+
     cout << "Nhap ten: ";
     cin >> name;
     users.push_back({id, name});
-    cout << "Dang ky thanh cong!\n";
-}
 
-void addRoute(int userId) {
-    string dest;
     cout << "Nhap diem den truong: ";
     cin >> dest;
-    routes.push_back({userId, dest});
-    cout << "Da them lo trinh!\n";
-}
+    routes.push_back({id, dest});
 
-void findMatches(int userId) {
+    cout << "Dang ky thanh cong!\n";
+
+    // Sau khi đăng ký, kiểm tra người phù hợp
     bool found = false;
-    for (auto &my : routes) {
-        if (my.userId == userId) {
-            for (auto &other : routes) {
-                if (other.userId != userId && other.destination == my.destination) {
-                    cout << "Ban dong hanh: " << users[other.userId - 1].name
-                         << " cung den " << other.destination << "\n";
-                    found = true;
-                }
-            }
+    for (auto &other : routes) {
+        if (other.userId != id && other.destination == dest) {
+            cout << "Tim thay ban dong hanh phu hop!\n";
+            cout << "Nguoi dung: " << users[other.userId - 1].name
+                 << " cung den " << dest << "\n";
+            found = true;
         }
     }
-    if (!found) cout << "Khong tim thay ban dong hanh.\n";
+    if (!found) {
+        cout << "Khong co nguoi phu hop.\n";
+    }
 }
 
 int main() {
-    int choice = 0;
-    int currentUser = -1;
+    int choice;
     bool running = true;
 
     while (running) {
-        cout << "\n--- MENU ---\n"
-             << "1. Dang ky | 2. Chon nguoi dung | 3. Them lo trinh | 4. Tim ban dong hanh | 5. Thoat\n"
-             << "Nhap lua chon: ";
+        cout << "\n--- MENU ---\n";
+        cout << "1. Dang ky (ten + lo trinh)\n";
+        cout << "2. Thoat\n";
+        cout << "Chon: ";
 
         if (!(cin >> choice)) {
             cin.clear();
             cin.ignore(1000, '\n');
-            cout << "Nhap sai! Vui long nhap so tu 1-5.\n";
+            cout << "Nhap sai! Vui long nhap so.\n";
             continue;
         }
 
         switch (choice) {
             case 1:
-                registerUser();
+                registerUserAndRoute();
                 break;
-            case 2: {
-                int id;
-                cout << "Nhap ID nguoi dung: ";
-                cin >> id;
-                if (id > 0 && id <= users.size()) {
-                    currentUser = id;
-                    cout << "Dang nhap voi user " << users[id - 1].name << "\n";
-                } else {
-                    cout << "Khong ton tai nguoi dung nay.\n";
-                }
-                break;
-            }
-            case 3:
-                if (currentUser != -1)
-                    addRoute(currentUser);
-                else
-                    cout << "Vui long chon nguoi dung truoc!\n";
-                break;
-            case 4:
-                if (currentUser != -1)
-                    findMatches(currentUser);
-                else
-                    cout << "Vui long chon nguoi dung truoc!\n";
-                break;
-            case 5:
+            case 2:
                 cout << "Tam biet!\n";
                 running = false;
                 break;
